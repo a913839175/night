6c86e55b28e79cfbad20537a036def44bd8b5a01
shenxinrong <shenxinrong@weipaidai.com>
2018-10-17 16:27:33 +0800

    sxr

---
+++ ewm.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>半夜说要改版头，我的内心几乎是崩溃的</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no" />
+	<style>
+		*{margin:0;padding:0;}
+		.main{padding:10%;}
+	</style>
+</head>
+<body>
+	<div class="main">
+		请用微信客户端打开<br /><br />
+		<img src="images/ewm.jpg" alt="" /><br /><br />
+		扫一扫或者长按识别二维码
+	</div>
+</body>
+</html>
